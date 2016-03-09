b0161f0fcc54f21c412ce2226cd71a34348af68e
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-09 20:05:56 +0000

    adds post title and description to post show page

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -9,4 +9,14 @@ describe 'navigate' do
     visit "/posts/#{@post.id}"
     expect(page.status_code).to eq(200)
   end
+  
+  it "shows the title of the post on the show page in an h1 tag" do 
+    visit "/posts/#{@post.id}"
+    expect(page).to have_css("h1", text: "My post")
+  end
+  
+  it "shows the description of the post on the show page in a p tag" do 
+    visit "/posts/#{@post.id}"
+    expect(page).to have_css("p", text: "My post desc")
+  end
 end
