4d23b3045d4db8bdb889e5fb8006fd908003a041
Amelie Oller <amelieoller@gmail.com>
2017-09-28 17:29:58 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,5 @@
 class PostsController < ApplicationController
-<<<<<<< HEAD
   def show
     @post = Post.find(params[:id])
   end
 end
-=======
-
-    def show
-        @post = Post.find(params[:id])
-    end
-
-end
->>>>>>> 5abc87db52e34f36af2aec22d5a5c70ddcccba1d
