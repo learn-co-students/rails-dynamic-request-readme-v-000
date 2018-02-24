0c86c466064c181f6c0ee344d5af195ac4fce8fc
Thomas Viles <thomas.c.viles.jr@gmail.com>
2018-02-24 18:56:00 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'posts__/no-test-framework'
+
+  get 'posts/:id', to: 'posts#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
