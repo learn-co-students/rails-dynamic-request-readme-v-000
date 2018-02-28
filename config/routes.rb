b8a959c5c6abe6c03433e9e376ca677660ca4e0a
rh24 <ms.rebeccahong@gmail.com>
2018-02-27 21:09:00 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  # get '/posts/:id', to: 'posts#show'
+  # replace with
+  resources :posts, only: :show
 end
