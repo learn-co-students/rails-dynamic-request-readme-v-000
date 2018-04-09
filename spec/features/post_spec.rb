66a3028068c8c8552572526f89fdad062a706ace
Robert Hughes <roberts.ginger.email@gmail.com>
2018-04-09 18:07:17 +0100

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
-    @post = Post.create(title: "My Post", description: "My post desc")
+    @post = Post.create(title: 'My Post', description: 'My post desc')
   end
 
   it 'loads the show page' do
@@ -12,11 +12,11 @@ describe 'navigate' do
 
   it 'shows the title on the show page in an h1 tag' do
     visit "/posts/#{@post.id}"
-    expect(page).to have_css("h1", text: "My Post")
+    expect(page).to have_css('h1', text: 'My Post')
   end
 
   it 'shows the description on the show page in a p tag' do
     visit "/posts/#{@post.id}"
-    expect(page).to have_css("p", text: "My post desc")
+    expect(page).to have_css('p', text: 'My post desc')
   end
 end
