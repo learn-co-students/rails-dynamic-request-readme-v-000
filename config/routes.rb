4e41e826db6d693c958bb70d5959deb561c6c66f
Everett <everett.bowline@gmail.com>
2018-10-01 02:20:27 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'about', to: "static#about"
 
-  get 'posts/:id', to: 'posts#show'
+  resources :posts, only: :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
