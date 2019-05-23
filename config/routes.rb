8307679ea999a5aa896443d4c7bb58ec0693f74e
Mohammed Noori <kmanoorib@gmail.com>
2019-05-23 23:25:03 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # there seems to be a few ways of setting up posts/:id
+  # below is the first way, but it is considered "long, drawn out" in the lesson
+  # get 'posts/:id', to: 'posts#show'
+  # refactor this by using Ruby's RESTful defaults and the resources method
+  resources :posts, only: :show
+  # this deals with one of the seven key RESTful routes
 end
