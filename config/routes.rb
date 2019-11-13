dfe234e0f7be324ca09aae4d706fd5ec8849e13c
github username <github email address>
2019-11-13 13:28:38 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'posts/:id', to: 'posts#show'
+  # get 'posts/:id', to: 'posts#show' this is a long drawn out route
 
+  resources :posts, only: :show
+  
 end
